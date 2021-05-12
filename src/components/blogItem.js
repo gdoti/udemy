23cdaa01c6ddfbf56c2fb92a833764e99a91be00
g.isowa <g.isowa@gmail.com>
2021-05-12 15:11:17 +0900

    add blogItem.js

---
+++ src/components/blogItem.js
@@ -0,0 +1,18 @@
+import { Link } from 'gatsby'
+import React from 'react'
+import { Card } from 'react-bootstrap'
+
+function BlogItem({title, date, src, link}) {
+    return (
+        <Card className="mt-4">
+            <Card.Img variant="top" src={src} />
+            <Card.Body>
+                <Card.Title>{title}</Card.Title>
+                <Card.Text>{date}</Card.Text>
+                <Link to={`/${link}`}>see more</Link>
+            </Card.Body>
+        </Card>
+    )
+}
+
+export default BlogItem
